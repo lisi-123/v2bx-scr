97fb78ee2e222ad5558d12a9600f2827073be322
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-07-24 15:18:31 +0800

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -16,7 +16,6 @@ until git clone https://github.com/lisi-123/v2bx-scr.git; do
 done
 
 # 赋予可执行权限
-chmod +x /root/v2bx-scr/socks5-check.sh
 chmod +x /root/clean_logs.sh
 
 # 检测并添加虚拟内存
@@ -39,29 +38,14 @@ wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh 
 # 修改为上海时区
 sudo timedatectl set-timezone Asia/Shanghai
 
-# 安装warp并设置本地socks5代理
-wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n5\n1\n1\n40000\n'
-
-# 限制warp内存占用
-sudo mkdir -p /etc/systemd/system/warp-svc.service.d && echo -e "[Service]\nMemoryMax=150M" | sudo tee /etc/systemd/system/warp-svc.service.d/override.conf
-sudo systemctl daemon-reexec
-sudo systemctl daemon-reload
-sudo systemctl restart warp-svc
-
 # 添加定时任务（凌晨4点自动重启v2bx，每分钟检测warp状态，自动清理vps日志）
 CRON_JOB1='0 4 * * * /usr/bin/v2bx restart'
-CRON_JOB2='* * * * * /root/v2bx-scr/socks5-check.sh'
 CRON_JOB3='0 5 * * * /root/v2bx-scr/clean_logs.sh'
 
 # 将任务添加到 crontab 并避免重复
 (crontab -l 2>/dev/null; echo "$CRON_JOB1"; echo "$CRON_JOB2"; echo "$CRON_JOB3") | sort -u | crontab -
 
-# 替换路由文件
-sudo cp -f /root/v2bx-scr/route.json /etc/V2bX/
-sudo cp -f /root/v2bx-scr/custom_outbound.json /etc/V2bX/
-sudo cp -f /root/v2bx-scr/sing_origin.json /etc/V2bX/
-
 V2bX restart
 
 # 输出完成信息
-echo "NodeID 已更新成功！已自动配置warp解锁，v2bx已启动"
+echo "NodeID 已成功安装v2bx！"
